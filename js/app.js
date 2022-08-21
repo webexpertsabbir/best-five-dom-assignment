c9c0611c9ed30cb2ea5c82f3234c98430f2fcdac
Sabbir <webexpertsabbir@gmail.com>
2022-08-22 05:13:07 +0600

    calculate total price

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// get input value
+function getInputField(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldStiring = inputField.value;
+    const carentInputField = parseFloat(inputFieldStiring);
+    inputField.value = '';
+    return carentInputField;
+
+}
+
+//get Text element function
+function getTextElement(elementId){
+    const textElement = document.getElementById(elementId);
+    const textElementStiring = textElement.innerText;
+    const carentTextElement = parseFloat(textElementStiring);
+    return carentTextElement;
+}
+
+// set element funtion
+function setCalculatePrice(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
+
+// calculate palyer price event handelar
+document.getElementById('player-total-price').addEventListener('click', function(){
+   const perPlayerPrice = getInputField('per-player-price');
+   if(isNaN(perPlayerPrice) == true){
+    alert('Pleace Input a Number');
+    return;
+   } 
+   const totalPlayerPrice = perPlayerPrice * 5;
+   setCalculatePrice('total-player-price', totalPlayerPrice);
+})
+
+// calculate total price event
+document.getElementById('calculate-total-price').addEventListener('click', function(){
+    const totalPlayerPrice = getTextElement('total-player-price');
+    const managerPrice = getInputField('manager-price');
+    const coachPrice = getInputField('coach-price');
+    if(isNaN(totalPlayerPrice) == true || isNaN(managerPrice) == true || isNaN(coachPrice) == true ){
+        alert('Pleace Input a Number');
+        return;
+       } 
+    const totalCalculatePrice = coachPrice + managerPrice + totalPlayerPrice;
+    setCalculatePrice('total-price', totalCalculatePrice);
+})
